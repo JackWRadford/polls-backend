80fda1cc7976683a4d0735192463c7e284b14d08
Jack Radford <jack.radford@live.co.uk>
2024-04-30 21:16:13 +0100

    Added mongodb and setup the connection. Changed the ES target and package type.

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,8 @@ import express from 'express'
 const port = process.env.PORT || 3000
 
 const app = express()
-
 app.use(express.json())
 
-app.get('/', (_, res) => {
-    res.send('Hello World!')
-})
 
 app.listen(port, () => {
     console.log(`Server listening at port: ${port}`)
