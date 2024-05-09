ae276c995a13ff4b306e51ced13fb057789d88b8
Jack Radford <jack.radford@live.co.uk>
2024-05-09 23:34:39 +0100

    Added and configured cors.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import "./loadEnv.js";
 import polls from "./routes/polls.js";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-// Configure Express Middleware.
+// Configure Express Middleware
 app.use(express.json());
+// Configure CORS
+const corsOptions = {
+	origin: process.env.FRONTEND_ORIGIN,
+	optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // Load the /polls routes
 app.use("/polls", polls);
