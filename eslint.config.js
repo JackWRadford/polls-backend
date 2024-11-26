062f66f0d150489113d0046804ef5e23e490a4c0
Jack <42590406+JackWRadford@users.noreply.github.com>
2024-11-26 22:31:59 +0000

    chore: fixed eslint-config-prettier import and formatted eslint config (#3)

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import { eslintConfigPerttier } from "eslint-config-prettier";
+import eslintConfigPerttier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts}"]
+    files: ["**/*.{js,mjs,cjs,ts}"],
   },
   {
-    languageOptions: { globals: globals.browser }
+    languageOptions: { globals: globals.browser },
   },
   {
     rules: {
       "no-unused-vars": "warn",
-      "no-undef": "warn"
-    }
+      "no-undef": "warn",
+    },
   },
-  eslintConfigPerttier
+  eslintConfigPerttier,
 ];
